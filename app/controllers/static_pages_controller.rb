da2cf618fa2002277cbeeb1f90e744b34cd85b88
Vostrikov2014 <Vostrikov2014@gmail.com>
2019-02-21 15:14:46 +0300

    Add a Static Pages controller
    Signed-off-by: Vostrikov2014 <Vostrikov2014@gmail.com>

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    render text:"MyCard24"
   end
 
   def help
+    render text:"Issue of cards Apple Wallet and Google Pay. Online surveys, push notifications, geofensing and iBeacon."
   end
 end
